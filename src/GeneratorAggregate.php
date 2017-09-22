df49512dbd10ffde8e5461da62012f901133a31b
Elan Ruusamäe <glen@delfi.ee>
2017-09-22 18:39:01 +0300

    generator+iterator = traversable

--- src/GeneratorAggregate.php
+++ src/GeneratorAggregate.php
@@ -3,23 +3,26 @@
 namespace Eventum\Delfi;
 
 use ArrayIterator;
-use Closure;
 use Generator;
 use IteratorAggregate;
+use Traversable;
 
 /**
  * Some kind of weird class to flatten Generators and Iterators
  *
  * It's really created to shortcome missing `yield from $generator` for PHP < 7.0
+ *
+ * @see http://php.net/manual/en/language.generators.syntax.php#control-structures.yield.from
+ * @see https://wiki.php.net/rfc/generator-delegation
  */
 class GeneratorAggregate implements IteratorAggregate {
 	/**
-	 * @var Generator|ArrayIterator $it
+	 * @var Traversable $it
 	 */
 	private $it;
 
 	/**
-	 * @param Generator|ArrayIterator $it
+	 * @param Traversable $it
 	 */
 	public function __construct($it) {
 		$this->it = $it;
@@ -39,12 +42,12 @@ class GeneratorAggregate implements IteratorAggregate {
 	/**
 	 * Iterate over $it, recurse if element is iterator or generator
 	 *
-	 * @param Generator|ArrayIterator $it
+	 * @param Traversable $it
 	 * @return Generator
 	 */
 	private function flatten($it) {
 		foreach ($it as $item) {
-			if ($item instanceof Generator || $item instanceof ArrayIterator) {
+			if ($item instanceof Traversable) {
 				foreach ($this->flatten($item) as $inner) {
 					yield $inner;
 				}
